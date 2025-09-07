4429e2a425d9f8d96f581c872f02d5cf68cdd646
Primary Personal/GA Tech <55899967+pixlflip@users.noreply.github.com>
2025-09-07 10:32:09 -0500

    Update args.sh

--- src/args.sh
+++ src/args.sh
@@ -72,7 +72,7 @@ export TARGET_UBUNTU_VERSION="oracular"
 # The default is the Aiursoft mirror.
 # You can change it to any other mirror that you prefer.
 # See https://docs.anduinos.com/Install/Select-Best-Apt-Source.html
-export BUILD_UBUNTU_MIRROR="https://mirror.enzu.com/ubuntu/"
+export BUILD_UBUNTU_MIRROR="http://ca.archive.ubuntu.com/ubuntu"
 
 # This is the name of the target OS.
 # Must be lowercase without special characters and spaces
